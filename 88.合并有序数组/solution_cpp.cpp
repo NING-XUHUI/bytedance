297718f62f1fc7e4c4adee1c9180580fde86d0b9
NING-XUHUI <892897986@qq.com>
2020-08-13 01:10:33 +0800

    .

---
+++ 88.合并有序数组/solution_cpp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    void merge(vector<int>& nums1,int m,vector<int>& nums2,int n){
+        int i = nums1.size() - 1;
+        m--;
+        n--;
+        while(n>=0){
+            if(m>=0 && nums1[m] >= nums2[n]){
+                swap(nums1[i--],nums1[m--]);
+            }else{
+                swap(nums1[i--],nums2[n--]);
+            }
+        }
+    }
+};
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
